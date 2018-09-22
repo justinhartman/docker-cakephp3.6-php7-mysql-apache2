fcaa581e9d02023cdbd2bd65ce25fbebe5ac9e4d
Justin Hartman <justin@hartman.me>
2018-09-22 20:28:27 +0200

    Revert "Added working Bionic Docker build for PHP 7.2."
    This reverts commit 720738740c23fac62e6e9b920edcaf56050d64ac.

--- php/apache/7.2/database.sql
+++
@@ -1,8 +0,0 @@
--- Create primary CakePHP database.
-CREATE DATABASE cakephp_live CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE USER cakephp_live_user@'localhost' IDENTIFIED BY 'b5uF95fstJmhABD4is';
-GRANT SELECT, INSERT, UPDATE ON cakephp_live.* TO 'cakephp_live_user'@'localhost';
--- Create test CakePHP database.
-CREATE DATABASE cakephp_test CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE USER cakephp_test_user@'localhost' IDENTIFIED BY '4hiEKuzgFr54fyPVQJ';
-GRANT SELECT, INSERT, UPDATE ON cakephp_test.* TO 'cakephp_test_user'@'localhost';
